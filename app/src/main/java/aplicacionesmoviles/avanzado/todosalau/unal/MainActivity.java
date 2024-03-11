2311f620ccfbdb7fb24b7aa448383c741eb8b65e
appsatenea_bog@unal.edu.co <appsatenea_bog@unal.edu.co>
2024-03-11 17:28:19 -0500

    Caracteristicas de dispositivos móviles con android

---
+++ app/src/main/java/aplicacionesmoviles/avanzado/todosalau/unal/MainActivity.java
@@ -0,0 +1,17 @@
+package aplicacionesmoviles.avanzado.todosalau.unal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
